27221d38db0beaab9beca68a324c42747a0a2c47
xlon72 <49961969+xlon72@users.noreply.github.com>
2026-08-31 23:13:49 +0800

    Update machine structure for SWGT024 V2.0 Unmanaged

--- machine.c
+++ machine.c
@@ -288,44 +288,44 @@ void machine_custom_init(void) { }
 
 #elif defined MACHINE_SWGT024_V2_0_UNMANAGED
 __code const struct machine machine = {
-	.machine_name = "SWGT024 V2.0 Unmanaged",
-	.isRTL8373 = 0,
-	.min_port = 3,
-	.max_port = 8,
-	.n_sfp = 2,
-	.log_to_phys_port = {0, 0, 0, 6, 1, 2, 3, 4, 5},
-	.phys_to_log_port = {4, 5, 6, 7, 8, 3, 0, 0, 0},
-	.is_sfp= {0, 0, 0, 2, 0, 0, 0, 0, 1},
-	// Left SFP port (J4)
-	.sfp_port[0].pin_detect = GPIO30_ACL_BIT3_EN,
-	.sfp_port[0].pin_los = GPIO37,
-	.sfp_port[0].pin_tx_disable = GPIO_NA,
-	.sfp_port[0].sds = 1,
-	.sfp_port[0].i2c = { .sda = GPIO39_I2C_SDA4, .scl = GPIO40_I2C_SCL3_MDC1 }, /* GPIO 39 */
-	// Right SFP port (J2)
-	.sfp_port[1].pin_detect = GPIO50_I2C_SCL2_UART1_TX,
-	.sfp_port[1].pin_los = GPIO51_I2C_SDA2_UART1_RX,
-	.sfp_port[1].pin_tx_disable = GPIO_NA,
-	.sfp_port[1].sds = 0,
-	.sfp_port[1].i2c = { .sda = GPIO41_I2C_SDA3_MDIO1, .scl = GPIO40_I2C_SCL3_MDC1 }, /* GPIO 40 */
-	.reset_pin = GPIO_NA,
-	.high_leds = { .mux = LED_27 | LED_29, .enable = LED_28_SYS | LED_29 },
-	.port_led_set = { 0, 0, 0, 1, 0, 0, 0, 0, 1},
-	.led_sets = {
-		{ /* RJ45: Green LED */
-			LEDS_1G | LEDS_100M | LEDS_10M | LEDS_LINK | LEDS_ACT | LEDS_10G,
-			0,
-			/* Amber LED */
-			LEDS_2G5 | LEDS_LINK | LEDS_ACT,
-			0
-		}, { /* SFP PORT: SINGLE GREEN LED */
-			LEDS_10M | LEDS_100M | LEDS_1G | LEDS_2G5 | LEDS_10G | LEDS_LINK | LEDS_ACT,
-			0,
-			0,
-			0,
+		.machine_name = "SWGT024 V2.0 Unmanaged",
+		.isRTL8373 = 0,
+		.min_port = 3,
+		.max_port = 8,
+		.n_sfp = 2,
+		.log_to_phys_port = {0, 0, 0, 6, 1, 2, 3, 4, 5},
+		.phys_to_log_port = {4, 5, 6, 7, 8, 3, 0, 0, 0},
+		.is_sfp= {0, 0, 0, 2, 0, 0, 0, 0, 1},
+		// Left SFP port (J4) — DISABLED for debug
+		.sfp_port[0].pin_detect = GPIO_NA,
+		.sfp_port[0].pin_los = GPIO_NA,
+		.sfp_port[0].pin_tx_disable = GPIO_NA,
+		.sfp_port[0].sds = 1,
+		.sfp_port[0].i2c = { .sda = GPIO39_I2C_SDA4, .scl = GPIO40_I2C_SCL3_MDC1 }, /* GPIO 39 */
+		// Right SFP port (J2) — DISABLED for debug
+		.sfp_port[1].pin_detect = GPIO_NA,
+		.sfp_port[1].pin_los = GPIO_NA,
+		.sfp_port[1].pin_tx_disable = GPIO_NA,
+		.sfp_port[1].sds = 0,
+		.sfp_port[1].i2c = { .sda = GPIO41_I2C_SDA3_MDIO1, .scl = GPIO40_I2C_SCL3_MDC1 }, /* GPIO 40 */
+		.reset_pin = GPIO_NA,
+		.high_leds = { .mux = LED_27 | LED_29, .enable = LED_28_SYS | LED_29 },
+		.port_led_set = { 0, 0, 0, 1, 0, 0, 0, 0, 1},
+		.led_sets = {
+			{ /* RJ45: Green LED */
+				LEDS_1G | LEDS_100M | LEDS_10M | LEDS_LINK | LEDS_ACT | LEDS_10G,
+				0,
+				/* Amber LED */
+				LEDS_2G5 | LEDS_LINK | LEDS_ACT,
+				0
+			}, { /* SFP PORT: SINGLE GREEN LED */
+				LEDS_10M | LEDS_100M | LEDS_1G | LEDS_2G5 | LEDS_10G | LEDS_LINK | LEDS_ACT,
+				0,
+				0,
+				0,
+			},
 		},
-	},
-};
+	};
 
 void machine_custom_init(void) { }
 
